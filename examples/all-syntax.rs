fc73f4326d8c69a0e0b6d272c44d96a3ffb8ee92
Jacob Birkett <jacob@birkett.dev>
2024-08-17 07:22:14 -0700

    examples: all-syntax: add more tests

--- examples/all-syntax.rs
+++ examples/all-syntax.rs
@@ -1,3 +1,4 @@
+use owo_colors::colors::css::{Orange, OrangeRed};
 use owo_colors::OwoColorize;
 use owo_format::format_args_colored;
 
@@ -20,17 +21,47 @@ macro_rules! logln {
 }
 
 fn main() {
-    logln!(:red "red");
-    logln!(:blue :on_red "blue", :fg_rgb::<255, 255, 255> "white" ; "bad" "ass");
-    let interpolation = "be interpolated";
-    logln!("concatenated" "text" 123 "stuff" {interpolation});
+    static EXAMPLE_DEBUG: Result<Option<usize>, ()> = Ok::<_, ()>(Some(usize::MAX));
+
+    // Basic coloring
+    logln!(:red "red text", :bold :blue "bold blue text");
+    logln!(:bold :red :on_blue "red on blue text");
+
+    // Custom coloring
+    logln!(:bold :fg::<Orange> :bg_rgb::<33, 50, 12> "custom foreground on custom background");
+    // format_args_colored!(:bold :color(Orange) :on_color((33, 50, 12)) "custom foreground on custom background");
+
+    // Concatenation
+    logln!("text" "concatenated" :red "without" "spaces");
+    logln!(:bold "commas", "concatenate", :cyan "with", "spaces");
+    logln!(:green "semicolons"; :red "will"; :underline :bold :cyan "break"; :magenta "lines");
+
+    // Debug can be done as a string literal
+    logln!("This is a debug format:", :purple "{EXAMPLE_DEBUG:?}");
+
+    // Bare identifiers should not work
+    // This is to disambiguate from function identifiers.
+    let fail = "fail bare identifier";
+    logln!(fail);
+
+    // Identifiers should be in an expression (either inline or block) or a borrow.
+    let ok = "identifier as expression or borrow";
+    logln!((ok); {ok}; &ok);
+
+    // Expressions don't need to have braces or parentheses.
     logln!(
-        :bold "bold", :black :on_white :italic "italic";
-        "new line" "and" "no space";
-        "words", "with", "spaces";
-        :bright_purple "nested invocations",
-        format_args_colored!(:bold :green "like this");
-        "let interpolation", :bold :underline :cyan interpolation;
-        :yellow "'" :italic "quoted text" :yellow "'"
+        "These are expressions:";
+        "Avagadro's number:", 6.02214076 * 10.0_f64.powi(23);
+        "This value was unwrapped twice:", :black :on_bright_blue
+            match EXAMPLE_DEBUG {
+                Ok(Some(max)) => max - (u32::MAX as usize),
+                _ => unreachable!("DEBUG is a static")
+            }
     );
+
+    // But they probably should.
+    logln!(:bright_cyan "a", :bright_red "+", :bright_cyan "b", :yellow "=", {
+        let (a, b) = (5, 5);
+        a + b
+    });
 }
